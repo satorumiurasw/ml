840743c91899b1d57b61ef0bdce3d7fa44dd7b14
Satoru Miura <satoru@miurasw.com>
2024-12-10 16:16:12 +0900

    推論機能を追加

---
+++ mlapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('input_form/', views.input_form, name='input_form'),
+    path('result/', views.result, name='result'),
+]
--- mlapp/views.py
+++ mlapp/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import InputForm
+import joblib
+import numpy as np
+from .models import Customer
 
-# Create your views here.
+
+loaded_model = joblib.load('model/ml_model.pkl')
+
+def index(request):
+    return render(request, 'mlapp/index.html')
+
+# def input_form(request):
+#     form = InputForm()
+#     return render(request, 'mlapp/input_form.html', {'form':form})
+def input_form(request):
+    if request.method == 'POST':
+        form = InputForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('result')
+    else:
+        form = InputForm()
+        return render(request, 'mlapp/input_form.html', {'form':form})
+    
+def result(request):
+    data = Customer.objects.order_by('id').reverse().values_list('limited_balance', 'education', 'marriage', 'age')
+    x = np.array([data[0]])
+    y = loaded_model.predict(x)
+    y_proba = loaded_model.predict_proba(x)
+    y_proba = y_proba*100
+    y, y_proba = y[0], y_proba[0]
+
+    customer = Customer.objects.order_by('id').reverse()[0]
+    customer.proba = y_proba[y]
+    customer.result = y
+    customer.save()
+
+    return render(request, 'mlapp/result.html', {'y':y, 'y_proba':round(y_proba[y], 2)})
